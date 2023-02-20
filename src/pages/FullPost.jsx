6d165cc99184c2a8e53caa5c7e117531e5aea8cd
vitalii <vilavren@gmail.com>
2023-02-21 03:37:02 +0500

    added a full post

--- src/pages/FullPost.jsx
+++ src/pages/FullPost.jsx
@@ -1,33 +1,49 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+
+import axios from '../axios'
 
 import { Post } from '../components/Post'
 import { Index } from '../components/AddComment'
 import { CommentsBlock } from '../components/CommentsBlock'
 
 export const FullPost = () => {
+  const [data, setData] = React.useState()
+  const [isLoading, setLoading] = React.useState(true)
+
+  const { id } = useParams()
+
+  React.useEffect(() => {
+    axios
+      .get(`/posts/${id}`)
+      .then((res) => {
+        setData(res.data)
+        setLoading(false)
+      })
+      .catch((err) => {
+        console.warn(err)
+        alert('Ошибка при получении статьи')
+      })
+  })
+
+  if (isLoading) {
+    return <Post isLoading={isLoading} isFullPost />
+  }
+
   return (
     <>
       <Post
-        id={1}
-        title="Тест статья #1 | JavaScript (JS)."
-        imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
-        user={{
-          avatarUrl:
-            'https://res.cloudinary.com/practicaldev/image/fetch/s--uigxYVRB--/c_fill,f_auto,fl_progressive,h_50,q_auto,w_50/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/187971/a5359a24-b652-46be-8898-2c5df32aa6e0.png',
-          fullName: 'Keff',
-        }}
-        createdAt={'12 июня 2022 г.'}
-        viewsCount={150}
+        id={data._id}
+        title={data.title}
+        imageUrl={data.imageUrl}
+        user={data.user}
+        createdAt={data.createdAt}
+        viewsCount={data.viewsCount}
         commentsCount={3}
-        tags={['react', 'fun', 'typescript']}
+        tags={data.tags}
         isFullPost
       >
-        <p>
-          JavaScript (JS) — высокоуровневый язык программирования, который
-          поддерживает императивный, функциональный, событийно-ориентированный и
-          другие подходы. Он относится к языкам с динамической типизацией,
-          входит в группу интерпретируемых языков.
-        </p>
+        <p>{data.text}</p>
       </Post>
       <CommentsBlock
         items={[
