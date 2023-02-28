381084707f8e94fe13a31dfb557544c25ab070fb
vitalii <vilavren@gmail.com>
2023-03-01 03:33:28 +0500

    added save of authorization token

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import Container from '@mui/material/Container'
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Routes, Route } from 'react-router-dom'
+
+import Container from '@mui/material/Container'
 
 import { Header } from './components'
 import { Home, FullPost, Registration, AddPost, Login } from './pages'
+import { fetchAuthMe, selectIsAuth } from './redux/slices/auth'
 
 function App() {
+  const dispatch = useDispatch()
+  const isAuth = useSelector(selectIsAuth)
+
+  React.useEffect(() => {
+    dispatch(fetchAuthMe())
+  })
+
   return (
     <>
       <Header />
--- src/redux/slices/auth.js
+++ src/redux/slices/auth.js
@@ -1,13 +1,15 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import axios from '../../axios'
 
-export const fetchAuth = createAsyncThunk(
-  'auth/fetchUserData',
-  async (params) => {
-    const { data } = await axios.post('/auth/login', params)
-    return data
-  }
-)
+export const fetchAuth = createAsyncThunk('auth/fetchAuth', async (params) => {
+  const { data } = await axios.post('/auth/login', params)
+  return data
+})
+
+export const fetchAuthMe = createAsyncThunk('auth/fetchAuthMe', async () => {
+  const { data } = await axios.get('/auth/me')
+  return data
+})
 
 const initialState = {
   data: null,
@@ -34,6 +36,20 @@ const authSlice = createSlice({
     },
 
     [fetchAuth.rejected]: (state) => {
+      state.status = 'error'
+      state.data = null
+    },
+    [fetchAuthMe.pending]: (state) => {
+      state.status = 'loading'
+      state.data = null
+    },
+
+    [fetchAuthMe.fulfilled]: (state, action) => {
+      state.status = 'loaded'
+      state.data = action.payload
+    },
+
+    [fetchAuthMe.rejected]: (state) => {
       state.status = 'error'
       state.data = null
     },
