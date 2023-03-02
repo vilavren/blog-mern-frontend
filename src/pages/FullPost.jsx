1dd7265af7a8121da9ab7302f7d351417af83b47
vitalii <vilavren@gmail.com>
2023-03-03 04:18:46 +0500

    added show post to home

--- src/pages/FullPost.jsx
+++ src/pages/FullPost.jsx
@@ -6,6 +6,7 @@ import axios from '../axios'
 import { Post } from '../components/Post'
 import { Index } from '../components/AddComment'
 import { CommentsBlock } from '../components/CommentsBlock'
+import ReactMarkdown from 'react-markdown'
 
 export const FullPost = () => {
   const [data, setData] = React.useState()
@@ -35,7 +36,7 @@ export const FullPost = () => {
       <Post
         id={data._id}
         title={data.title}
-        imageUrl={data.imageUrl}
+        imageUrl={`http://localhost:4444${data.imageUrl}`}
         user={data.user}
         createdAt={data.createdAt}
         viewsCount={data.viewsCount}
@@ -43,7 +44,7 @@ export const FullPost = () => {
         tags={data.tags}
         isFullPost
       >
-        <p>{data.text}</p>
+        <ReactMarkdown children={data.text} />
       </Post>
       <CommentsBlock
         items={[
