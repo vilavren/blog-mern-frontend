adce68b08d005fcfb082328aaf1d2d5a47b43fc9
vitalii <vilavren@gmail.com>
2023-03-03 04:03:24 +0500

    added create post

--- src/pages/AddPost/index.jsx
+++ src/pages/AddPost/index.jsx
@@ -1,23 +1,70 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { Navigate, useNavigate } from 'react-router-dom'
+
 import TextField from '@mui/material/TextField'
 import Paper from '@mui/material/Paper'
 import Button from '@mui/material/Button'
 import SimpleMDE from 'react-simplemde-editor'
-
 import 'easymde/dist/easymde.min.css'
 import styles from './AddPost.module.scss'
 
+import { selectIsAuth } from '../../redux/slices/auth'
+import axios from '../../axios'
+
 export const AddPost = () => {
-  const imageUrl = ''
-  const [value, setValue] = React.useState('')
+  const isAuth = useSelector(selectIsAuth)
+  const navigate = useNavigate()
 
-  const handleChangeFile = () => {}
+  const [isLoading, setLoading] = React.useState(false)
+  const [text, setText] = React.useState('')
+  const [title, setTitle] = React.useState('')
+  const [tags, setTags] = React.useState('')
+  const [imageUrl, setImageUrl] = React.useState('')
 
-  const onClickRemoveImage = () => {}
+  const inputFileRef = React.useRef(null)
 
-  const onChange = React.useCallback((value) => {
-    setValue(value)
+  const handleChangeFile = async (event) => {
+    try {
+      const formData = new FormData()
+      const file = event.target.files[0]
+
+      formData.append('image', file)
+      const { data } = await axios.post('/upload', formData)
+      setImageUrl(data.url)
+    } catch (err) {
+      console.warn(err)
+      alert('Ошибка при загрузке файла!')
+    }
+  }
+
+  const onClickRemoveImage = () => {
+    setImageUrl('')
+  }
+
+  const onChange = React.useCallback((text) => {
+    setText(text)
   }, [])
+
+  const onSubmit = async () => {
+    try {
+      setLoading(true)
+
+      const fields = {
+        title,
+        text,
+        tags: tags.split(','),
+        imageUrl,
+      }
+
+      const { data } = await axios.post('/posts', fields)
+      const id = data._id
+      navigate(`/posts/${id}`)
+    } catch (err) {
+      console.warn(err)
+      alert('Ошибка при создании статьи!')
+    }
+  }
 
   const options = React.useMemo(
     () => ({
@@ -34,23 +81,40 @@ export const AddPost = () => {
     []
   )
 
+  if (!window.localStorage.getItem('token') && !isAuth) {
+    return <Navigate to="/" />
+  }
+
   return (
     <Paper style={{ padding: 30 }}>
-      <Button variant="outlined" size="large">
+      <Button
+        onClick={() => inputFileRef.current.click()}
+        variant="outlined"
+        size="large"
+      >
         Загрузить превью
       </Button>
-      <input type="file" onChange={handleChangeFile} hidden />
+      <input
+        ref={inputFileRef}
+        type="file"
+        onChange={handleChangeFile}
+        hidden
+      />
       {imageUrl && (
-        <Button variant="contained" color="error" onClick={onClickRemoveImage}>
-          Удалить
-        </Button>
-      )}
-      {imageUrl && (
-        <img
-          className={styles.image}
-          src={`http://localhost:4444${imageUrl}`}
-          alt="Uploaded"
-        />
+        <>
+          <Button
+            variant="contained"
+            color="error"
+            onClick={onClickRemoveImage}
+          >
+            Удалить
+          </Button>
+          <img
+            className={styles.image}
+            src={`http://localhost:4444${imageUrl}`}
+            alt="Uploaded"
+          />
+        </>
       )}
       <br />
       <br />
@@ -59,21 +123,25 @@ export const AddPost = () => {
         variant="standard"
         placeholder="Заголовок статьи..."
         fullWidth
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
       />
       <TextField
         classes={{ root: styles.tags }}
         variant="standard"
         placeholder="Тэги"
         fullWidth
+        value={tags}
+        onChange={(e) => setTags(e.target.value)}
       />
       <SimpleMDE
         className={styles.editor}
-        value={value}
+        value={text}
         onChange={onChange}
         options={options}
       />
       <div className={styles.buttons}>
-        <Button size="large" variant="contained">
+        <Button onClick={onSubmit} size="large" variant="contained">
           Опубликовать
         </Button>
         <a href="/">
--- src/pages/FullPost.jsx
+++ src/pages/FullPost.jsx
@@ -24,7 +24,7 @@ export const FullPost = () => {
         console.warn(err)
         alert('Ошибка при получении статьи')
       })
-  })
+  }, [])
 
   if (isLoading) {
     return <Post isLoading={isLoading} isFullPost />
